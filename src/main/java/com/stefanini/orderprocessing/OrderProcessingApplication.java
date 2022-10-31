78af22552d6ce36284b144321c8c6045ce3905c9
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-10-31 17:29:04 +0200

    fixed error with datasource

--- src/main/java/com/stefanini/orderprocessing/OrderProcessingApplication.java
+++ src/main/java/com/stefanini/orderprocessing/OrderProcessingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class OrderProcessingApplication {
 
 	public static void main(String[] args) {
