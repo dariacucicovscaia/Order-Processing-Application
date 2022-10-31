81ce0c7f55e94fb95bef696afdf9d9c2550a45c5
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-10-31 12:46:02 +0200

    from org.springframework.jdbc.* to  java.sql*

--- src/main/java/com/stefanini/orderprocessing/dao/impl/OrderDAOImpl.java
+++ src/main/java/com/stefanini/orderprocessing/dao/impl/OrderDAOImpl.java
@@ -3,14 +3,12 @@ package com.stefanini.orderprocessing.dao.impl;
 import com.stefanini.orderprocessing.dao.OrderDAO;
 import com.stefanini.orderprocessing.domain.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class OrderDAOImpl extends GenericDAOAbstractImpl<Order> implements OrderDAO<Order> {
     @Autowired
-    public OrderDAOImpl(JdbcTemplate jdbcTemplate) {
-        super(jdbcTemplate);
+    public OrderDAOImpl() {
         setClazz(Order.class);
     }
 }
